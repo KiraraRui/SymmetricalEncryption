b29a2b7df81c3fd3fe831834a3c6fb8da91ebfa6
ShizukuKiri <kira.kira@live.dk>
2021-04-21 17:38:57 +0200

    Add files via upload

---
+++ SymmetricalEncryption/Encrypter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SymmetricalEncryption
+{
+    public class Encrypter
+    {
+        
+    public SymmetricAlgorithm MyCipherAlg { get; private set; }
+
+    public Encrypter(string cipher)
+        {
+            switch (cipher)
+            {
+                case "DES":
+                    MyCipherAlg = DES.Create();
+                    break;
+
+                case "TRIPLEDES":
+                    MyCipherAlg = TripleDES.Create();
+                    break;
+
+                case "AES":
+                    MyCipherAlg = Rijndael.Create();
+                    break;
+
+            }
+            MyCipherAlg.GenerateKey();
+            MyCipherAlg.GenerateIV();
+        }
+
+
+        //Create an aesmanaged object with the specified key/iv
+        public byte[] Encrypt(byte[] input)
+        {
+
+                //myCipherAlg.Key = key;
+                //myCipherAlg.IV = iv;
+
+                //create an encryptor to perform the stream transform
+                ICryptoTransform cryptoTransform = MyCipherAlg.CreateEncryptor(); //(myCipherAlg.Key, myCipherAlg.IV);
+
+            // As used in the example in the document of the assignment i have choosen to use using here as well
+            // the using statement defines our scope at the end of which an object will be disposed
+            //create te streams used for encrypption
+            using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                    cryptoStream.Write(input, 0, input.Length);
+                    }
+                return memoryStream.ToArray();
+                }
+            }
+        
+        // legit the sane but with decrypt instead
+        public byte[] Decryption(byte[] input, byte[] key, byte[] iv)
+        {
+            byte[]plaintext = new byte[input.Length];
+            MyCipherAlg.Key = key;
+            MyCipherAlg.IV = iv;
+
+                ICryptoTransform cryptoTransform = MyCipherAlg.CreateDecryptor();
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Read))
+                    {
+                        cryptoStream.Read(plaintext, 0,  input.Length);
+                        cryptoStream.FlushFinalBlock();
+
+                    }
+                        return plaintext;
+                }
+            }
+        
+        //Can be used if custom keys are used in the future
+        //public byte[] GenRNG(int length)
+        //{
+        //    using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        //    {
+        //        var randomNumber = new byte[length];
+        //        randomNumberGenerator.GetBytes(randomNumber);
+
+        //        return randomNumber;
+        //    }
+        //}
+    }
+}
---
+++ SymmetricalEncryption/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+
+namespace SymmetricalEncryption
+{
+    public class Program
+    {
+        private static Encrypter encrypter;
+
+        private static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Symmectric Encryption & Decryption\n");
+
+                Console.WriteLine(@"
+                         +------------------------------------+
+                         | +------------------------------------+
+                         | |                                  | |
+                         | |        Press 1 for DES           | |
+                         | |      Press 2 for TripleDES       | |
+                         | |         Press 3 for AES          | |
+                         | |         Press 4 to EXIT          | |
+                         | |    ==========================    | |
+                         | |      Remember to press enter     | |
+                         | |                                  | |
+                         +------------------------------------+ |
+                           +------------------------------------+ ");
+
+                // Note to self : Console.Clear();
+
+                string userInput = Console.ReadLine();
+
+                if (userInput == "1")
+                {
+                    encrypter = new Encrypter("DES");
+                    GetInput("DES");
+                }
+                else if (userInput == "2")
+                {
+                    encrypter = new Encrypter("TRIPLEDES");
+                    GetInput("TrippleDES");
+                }
+                else if (userInput == "3")
+                {
+                    encrypter = new Encrypter("AES");
+                    GetInput("AES");
+                }
+                else if (userInput == "4")
+                {
+                    isRunning = false;
+                }
+                //this one is never shown, fix
+                else
+                {
+                    Console.WriteLine("Please choose an option");
+                }
+            }
+        }
+
+        private static void GetInput(string algorithm)
+        {
+            string message;
+            byte[] key;
+            byte[] iv;
+
+            Console.Clear();
+            Console.WriteLine($"{algorithm} Encryption & Decryption\n");
+
+            Console.WriteLine(@"
+                         +------------------------------------+
+                         | +------------------------------------+
+                         | |                                  | |
+                         | |        Press 1 to Encrypt        | |
+                         | |        Press 2 to Decrypt        | |  
+                         | |        Press 4 to go back        | |
+                         | |    ==========================    | |
+                         | |      Remember to press enter     | |
+                         | |                                  | |
+                         +------------------------------------+ |
+                           +------------------------------------+ ");
+            string choosenUserInput = Console.ReadLine();
+
+
+            if (choosenUserInput == "1")
+            {
+                Console.Clear();
+                Console.Write("Messsage: ");
+                message = Console.ReadLine();
+
+               byte[] encrypted = encrypter.Encrypt(Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($"\r\nYour decrypted message: {encrypter.Decryption(encrypted, encrypter.MyCipherAlg.Key, encrypter.MyCipherAlg.IV)}");
+                Console.WriteLine($"\nYour encrypted message: {Convert.ToBase64String(encrypted)}");
+                Console.WriteLine($"\nYour key: {Convert.ToBase64String(encrypter.MyCipherAlg.Key)}");
+                Console.WriteLine($"The IV: {Convert.ToBase64String(encrypter.MyCipherAlg.IV)}");
+                Console.WriteLine("Plz press enter for continuing");
+                Console.ReadLine();
+            }
+
+            else if (choosenUserInput == "2")
+            {
+                Console.Clear();
+                Console.Write("messsage: ");
+                message = Console.ReadLine();
+
+                Console.Write("\nThe keey: ");
+                key = Convert.FromBase64String(Console.ReadLine());
+                Console.WriteLine(key.Length);
+                Console.Write("\nThe IV: ");
+                iv = Convert.FromBase64String(Console.ReadLine());
+
+                Console.WriteLine($"\r\nYour decrypted message: {encrypter.Decryption(Convert.FromBase64String(message), key, iv)}");
+                Console.WriteLine("Plz press enter for continuing");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please choose an option");
+            }
+
+        }
+    }
+}
+
+
